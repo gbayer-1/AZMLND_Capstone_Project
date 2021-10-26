2a2c557a81cee9acbb7d313b8cd7246083cbd2ac
gbayer-1 <92030321+gbayer-1@users.noreply.github.com>
2021-10-26 11:27:00 +0000

    add hyperdrive_model folder

---
+++ hyperdrive_model/write_env_file.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 09:44:11 2021
+
+@author: GABRIFR
+"""
+
+from azureml.core.conda_dependencies import CondaDependencies
+
+myenv = CondaDependencies.create(pip_packages=["numpy",
+                                               "pandas",
+                                               "onnxruntime",
+                                               "azureml-core",
+                                               "azureml-defaults"])
+
+with open("hyperdrive_env.yml","w") as f:
+    f.write(myenv.serialize_to_string())
